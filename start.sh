57e34251185997516b1b8d75b788f7966000a906
Admin <admin@z-OS.ibm.lan>
2022-10-05 16:30:10 +0300

    updated the bash

--- start.sh
+++ start.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# 
+# to do: make this in python
 
 systemctl status docker.service 
 systemctl enable docker.service 
 systemctl start docker.service 
 systemctl status docker.service 
 
+docker start test-db
